00b3566333023cd5a67d3fbc8a5636d3b74207b2
mhewb <mhe.bousquet@gmail.com>
2023-05-17 09:13:16 +0200

    init commit

---
+++ src/main/java/io/m2i/TPInvoice/TpInvoiceApplication.java
@@ -0,0 +1,13 @@
+package io.m2i.TPInvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpInvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/m2i/TPInvoice/TpInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.m2i.TPInvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
